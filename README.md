1ef330a9c7cff08e96eb30ef484747e920850d48
kchapelier <kevin.chapelier@gmail.com>
2014-02-02 09:45:35 +0100

    full implementation and doc

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
-node-glitch
-===========
+# node-glitch
 
 Glitched stream for intentional data corruption.
+
+## install
+
+With [npm](http://npmjs.org) do:
+
+```
+npm install glitch
+```
+
+## API reference
+
+### glitch(fileSrc, fileDst, probability, deviation, mode)
+
+* fileSrc : Source file.
+* fileDst : Destination file.
+* probability : Probability of deviation per byte (between 0 and 1).
+* deviation : Maximum value deviation per byte.
+* mode : 
+
+### new glitch.GlitchedStream(options);
+
+Transform stream implementation.
+
+Options :
+
+* probability : Probability of deviation per byte (between 0 and 1).
+* deviation : Maximum value deviation per byte.
+* whiteList : Array of whitelisted values, if set only bytes with those values will be modified.
+* blackList : Array of blacklisted values, no byte with those values will be modified.
+* deviationFunction : Replace the default deviation function of the stream, allows to write more complex filtering than whitelisting and blacklisting
+
+## Examples
+
+```js
+var glitch = require('glitch');
+glitch('test.jpg', 'test.glitched.jpg', 0.0001, 2, 0777);
+```
+
+Copy a glitched version of test.jpg as test.glitched.jpg where each byte has probability of 1/10000 of having its value modified by a maximum of 2.
+
+```js
+var GlitchedStream = require('glitch').GlitchedStream;
+var gstream = new GlitchedStream({
+  'deviation' : 1,
+  'probability' : 1,
+  'whiteList' : [0xcc,0xcd,0xce,0xcf]
+});
+someMidiStream.pipe(gstream).pipe(someMidiReceiver);
+```
+
+Pipe a midi stream into a glitched stream which will modify every bytes matching the whitelist by a maximum value of 1 before piping it into midiReceiver.
+
+## Potential use case
+
+* Glitching text files and images (though you'll end up with a lot of totally unreadable files).
+* Testing applications or libraries against failure.
+* Celebrating april's fool day at the expense of a grunt/gulp user.
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -1,72 +1,100 @@
 var
-	fs = require('fs'),
-	util = require('util'),
-	stream = require('stream');
+  fs = require('fs'),
+  util = require('util'),
+  stream = require('stream');
 
+/**
+ * GlitchedStream, a Transform stream to intentionally corrupt data
+ * @param {Object} options
+ * @constructor
+ */
 var GlitchedStream = function(options) {
-	stream.Transform.call(this, options);
-	
-	this.frequency = Math.max(options.frequency || 60, 1);
-	this.deviation = Math.max(options.deviation || 30, 1);
+  "use strict";
+  stream.Transform.call(this, options);
+
+  options = options || {};
+
+  this.probability = options.probability || 0;
+  this.deviation = Math.abs(options.deviation || 0);
+
+  this.whiteList = options.whiteList || [];
+  this.blackList = options.blackList || [];
+  this.func = options.func;
 };
 
 util.inherits(GlitchedStream, stream.Transform);
 
+GlitchedStream.prototype.probability = null;
+GlitchedStream.prototype.deviation = null;
+GlitchedStream.prototype.whiteList = null;
+GlitchedStream.prototype.blackList = null;
+GlitchedStream.prototype.func = null;
+
+/**
+ * Apply the deviation
+ * @param {Number} value - Value
+ * @returns {Number} New value
+ * @private
+ */
 GlitchedStream.prototype.deviate = function(value) {
-	var newVal = Math.max(0, Math.min(255, value + Math.floor((Math.random() - 0.5) * this.deviation)));
-	return newVal
+  "use strict";
+
+  return value + Math.round((Math.random() * 2 - 1) * this.deviation);
 };
 
+/**
+ * Transformation method
+ * @param {Buffer} chunk - Binary buffer
+ * @param {String} enc - Encoding
+ * @param {Function} callback - Callback
+ * @private
+ */
 GlitchedStream.prototype._transform = function(chunk, enc, callback) {
-	for(var i = 0, l = chunk.length; i < l; i++) {
-		if(chunk[i] === 0xff && Math.random() * this.frequency > this.frequency - 1) {
-			chunk[i] = this.deviate(chunk[i]);
-		}
-		
-		if(chunk[i] === 0x0f && Math.random() * this.frequency > this.frequency - 1) {
-			chunk[i] = this.deviate(chunk[i]);
-		}
-		
-		if(chunk[i] === 0xc0 && Math.random() * this.frequency > this.frequency - 1) {
-			chunk[i] = this.deviate(chunk[i]);
-		}
-		
-		if(chunk[i] === 0xee && Math.random() * this.frequency > this.frequency - 1) {
-			chunk[i] = this.deviate(chunk[i]);
-		}
-		
-		if(chunk[i] === 0xe2 && Math.random() * this.frequency > this.frequency - 1) {
-			chunk[i] = this.deviate(chunk[i]);
-		}
-		if(chunk[i] === 0xe1 && Math.random() * this.frequency > this.frequency - 1) {
-			chunk[i] = this.deviate(chunk[i]);
-		}
-		
-		if(chunk[i] === 0xe0 && Math.random() * this.frequency > this.frequency - 1) {
-			chunk[i] = this.deviate(chunk[i]);
-		}
-		
-		if(chunk[i] === 0x2f && Math.random() * this.frequency > this.frequency - 1) {
-			chunk[i] = this.deviate(chunk[i]);
-		}
-	}
-	
-	this.push(chunk);
-	callback();
+  "use strict";
+
+  var deviate = this.func || this.deviate.bind(this);
+
+  for(var i = 0, l = chunk.length; i < l; i++) {
+    var currentValue = chunk[i];
+
+    if(this.whiteList.length > 0 && this.whiteList.indexOf(currentValue) < 0) {
+      continue;
+    }
+
+    if(this.blackList.length > 0 && this.blackList.indexOf(currentValue) >= 0) {
+      continue;
+    }
+
+    if(Math.random() >= this.probability) {
+      continue;
+    }
+
+    chunk[i] = Math.max(0, Math.min(255, deviate(currentValue, this.deviation)));
+  }
+
+  this.push(chunk);
+  callback();
 };
 
-//TODO clear all hardcoded values
-//TODO should try to catch the errors or let the user deal with it ?
+/**
+ * Read the content of a file, corrupt it and write it into another file
+ * @param {String} fileSrc - Path of the source file
+ * @param {String} fileDst - Path of the destination file
+ * @param {Number} probability - Probability of deviation per byte (between 0 and 1)
+ * @param {Number} deviation - Maximum value difference between each byte of the source and of the destination
+ * @param {Number} mode
+ */
+var glitch = function(fileSrc, fileDst, probability, deviation, mode) {
+  "use strict";
+  var rstream = fs.createReadStream(fileSrc);
+  var woptions = mode ? { 'mode' : mode } : {};
+  var wstream = fs.createWriteStream(fileDst, woptions);
+  var gstream = new GlitchedStream({
+    'probability' : probability,
+    'deviation' : deviation
+  });
 
-var glitch = function(file, num, frequency, deviation, extension) {
-	var rstream = fs.createReadStream(file);
-	var wstream = fs.createWriteStream('output/test.' +  num + '.' + extension, { 'mode' : 0777 });
-	var gstream = new GlitchedStream({
-		'frequency' : frequency,
-		'deviation' : deviation
-	});
-	
-	rstream.pipe(gstream).pipe(wstream);
+  rstream.pipe(gstream).pipe(wstream);
 };
 
 glitch.GlitchedStream = GlitchedStream;
